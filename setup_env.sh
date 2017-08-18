738ba859ec9c862e4b798ced139a5f592866f25a
Tomasz Fiechowski <tomasz.fiechowski@codility.com>
2017-08-18 16:19:58 +0200

    Initial script for setting up env

---
+++ setup_env.sh
@@ -0,0 +1,7 @@
+# Installing git bash completion
+brew install bash-completion
+
+# This should be appended to .bash_profile
+# if [ -f $(brew --prefix)/etc/bash_completion ]; then
+#     . $(brew --prefix)/etc/bash_completion
+# fi
